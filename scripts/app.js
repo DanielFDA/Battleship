9b90c6ca20f9a428fbaf064f72aa0bbdad307dcd
Daniel Fernandez De Ascencao <dfernandezda21@gmail.com>
2020-11-20 01:35:31 +0000

    cantdoitnomore1-Board Placement or at least thats what i was trying

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,61 @@
 
 function init() {
   console.log('js is up and running')
+  // ! elements
+
+  const width = 11
+  const trackingGrid = document.querySelector('#trackingGrid')
+  const oceanGrid = document.querySelector('#oceanGrid')
+  const cellCount = width * width 
+  const oceanGridCells =  []
+  const trackingGridCells = []
+  console.log(trackingGridCells)
+
+
+
+  function createTrackingGridsCells() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      trackingGrid.appendChild(cell).classList.add('gridsCells')
+      trackingGridCells.push(cell)
+    }
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      oceanGrid.appendChild(cell).classList.add('gridsCells')
+      oceanGridCells.push(cell)
+    }
+  }
+
+  createTrackingGridsCells()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
