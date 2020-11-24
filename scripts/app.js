851cdf19035162a9382ff0c61fa4bcab8b2678e2
Daniel Fernandez De Ascencao <dfernandezda21@gmail.com>
2020-11-24 22:58:50 +0000

    randomShips generated on both boards and the beggining of the logic

--- scripts/app.js
+++ scripts/app.js
@@ -48,6 +48,9 @@ function init() {
     }
   ]
 
+  let shotFired 
+  let cellUnderFire
+
   // ! Functions
 
 
@@ -67,18 +70,18 @@ function init() {
 
 
   function oceanGridShipsGenerator(ship) {
-    let randomDirection = Math.floor(Math.random() * ship.directions.length)
-    let current = ship.directions[randomDirection]
-    let direction 
+    let randomDirection = Math.floor(Math.random() * ship.directions.length) // te devuelve un valor random 0 o 1
+    let current = ship.directions[randomDirection] // elige una de las dos direcciones q tiene el array
+    let direction  // se usa para luego multiplicar dependiendo de si es horizontal o vertical
     if (randomDirection === 0) direction = 1
     if (randomDirection === 1) direction = 10
-    let randomStart = Math.abs(Math.floor(Math.random() * oceanGridCells.length - (ship.directions[0].length * direction)))
+    let randomStart = Math.abs(Math.floor(Math.random() * oceanGridCells.length - (ship.directions[0].length * direction)))  // agarra una cell random del oceangrid y le resta el largo de las direcciones del barco q va a crear (multiplicado por la direccion(horizontal o vertical)) ej: un destroyer seria 36 - (3 * 1(si es horizontal) o 10(si es vertical)) en el caso de ser vertical tienes 36 - 3*10 = 6, la primera posicion del barco seria la cell numero 6
 
-    const isTaken = current.some(index => oceanGridCells[randomStart + index].classList.contains('cellTaken'))
-    const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
-    const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+    const isTaken = current.some(index => oceanGridCells[randomStart + index].classList.contains('cellTaken')) // esta funcion se encarga de revisar si la random cell ya tiene asignada la clase de cellTaken
+    const isAtRightEdge = current.some(index => (randomStart + index) % width > width - 1) // esta funcion se encarga de buscar tu cell de inicio e irle agregando todas las cells del barco y comprobando q esten en el borde derecho. ej: randomStart(36) + index(depende de las vidas del barco y de si es horizontal(0,1,2) o vertical(0,width,width*2) ) en el caso del destroyer seria 36+0)%10 = 6 === 9; 36+1)%10 = 7 === 9; 36+2) %10 = 8 === 9. // ! lo cambie para que sea menor enves de estrictamente igual, de esta manera pueden aparecer barcos en la ultima fila de la derecha, pero no logro hacer que puedan aparecer tambien en la de la izquierda porque me genera un error que hace q los barcos salgan desde un extremo de una linea hasta el otro extremo en la siguiente linea
+    const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0) // lo mismo de arriba solo que para el lado izquierdo 
 
-    if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => oceanGridCells[randomStart + index].classList.add('cellTaken', ship.name))
+    if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => oceanGridCells[randomStart + index].classList.add('cellTaken', ship.name)) // se asegura q todos los casos anteriores sean falsos para agregar randommente los barcos y agregarles dos clases una de celltaken y otra con su nombre
 
     else oceanGridShipsGenerator(ship)
   }
@@ -98,7 +101,7 @@ function init() {
     let randomStart = Math.abs(Math.floor(Math.random() * trackingGridCells.length - (ship.directions[0].length * direction)))
 
     const isTaken = current.some(index => trackingGridCells[randomStart + index].classList.contains('cellTaken'))
-    const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
+    const isAtRightEdge = current.some(index => (randomStart + index) % width > width - 1)
     const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
 
     if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => trackingGridCells[randomStart + index].classList.add('cellTakenByAI', ship.name))
@@ -113,8 +116,19 @@ function init() {
   trackingGridShipsGenerator(ships[4])
 
 
-
-
+  trackingGridCells.forEach(cell => cell.addEventListener('click', function(e) {
+    shotFired = cell.dataset.id // encontramos el dataset id de la cell que hacemos click
+    // revealSquare(square.classList)
+    const query = `[data-id="${shotFired}"]`    // transformamos el dataset id en un string para poder encontrarlo con queryselector
+    cellUnderFire = trackingGrid.querySelector(query)  // asigna el elemnto q fue clickeado usando un queryselect para el dataset id dentro del tracking grid
+    cellUnderFire.classList.add('cellShot')
+    if (cellUnderFire.classList.contains('cellShot')) {
+      console.log('ns')
+    }
+  }))
+  // const query = `[data-id=${shotFired}]`
+  // const resp = trackingGrid.querySelector(query)
+  // console.log(resp)
 
 
 
